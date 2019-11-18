1f90559cb4e9565498b0b23e814de781e44e7ae2
Christian Arty <artychristian@gmail.com>
2019-11-18 17:33:57 -0500

    Implemented the searchLogs functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   UPDATE_LOG,
   SET_CURRENT,
-  CLEAR_CURRENT
+  CLEAR_CURRENT,
+  SEARCH_LOGS
 } from "./types";
 
 // export const getLogs = () => {
@@ -100,6 +101,24 @@ export const updateLog = log => async dispatch => {
     });
   }
 };
+//Search server logs
+export const searchLogs = (text) => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
 //set current log
 export const setCurrent = log => {
   return {
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -6,6 +6,7 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
+  SEARCH_LOGS,
   UPDATE_LOG
 } from "../actions/types";
 
@@ -42,6 +43,12 @@ export default (state = initialState, action) => {
         logs: state.logs.map(log =>
           log.id === action.payload.id ? action.payload : log
         ),
+        loading: false
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
         loading: false
       };
     case SET_LOADING:
