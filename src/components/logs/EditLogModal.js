d48e117a4e9dbce2f3828d89bafff49b6524706a
Christian Arty <artychristian@gmail.com>
2019-11-18 17:25:25 -0500

    Full CRUD functionality for logs

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import M from "materialize-css/dist/js/materialize.min.js";
+import PropTypes from "prop-types";
+import { updateLog, clearCurrent } from "../../actions/logActions";
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog, clearCurrent}) => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
 
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    }
+  }, [current]);
+
   const reset = () => {
-    setMessage('');
-    setTech('');
+    setMessage("");
+    setTech("");
     setAttention(false);
-  }
-  
+  };
+
   const onSubmit = () => {
     if (message === "" || tech === "") {
       M.toast({ html: "Please enter a message and tech" });
     } else {
-      console.log(message, tech, attention);
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+      updateLog(updLog);
+      M.toast({ html: `Log updated by ${tech}` });
     }
+    clearCurrent();
     //Clear Fields
     reset();
   };
@@ -34,9 +54,6 @@ const EditLogModal = () => {
               value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor="message" className="active">
-              Log Message
-            </label>
           </div>
         </div>
         <div className="row">
@@ -87,9 +104,19 @@ const EditLogModal = () => {
   );
 };
 
+const mapStateToProps = state => ({
+  current: state.log.current
+});
+
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired,
+  clearCurrent: PropTypes.func.isRequired,
+};
+
 const modalStyle = {
   width: "75%",
   height: "75%"
 };
 
-export default EditLogModal;
+export default connect(mapStateToProps, { updateLog, clearCurrent })(EditLogModal);
