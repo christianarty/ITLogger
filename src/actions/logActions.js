45ccf171ffd0938168c24df0c4951a56f003419c
Christian Arty <artychristian@gmail.com>
2019-11-18 16:58:25 -0500

    Added the delete log from Redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG
+} from "./types";
 
 // export const getLogs = () => {
 //   return async dispatch => {
@@ -27,7 +33,7 @@ export const getLogs = () => async dispatch => {
     });
   }
 };
-
+// add log to database
 export const addLog = log => async dispatch => {
   try {
     setLoading();
@@ -42,6 +48,24 @@ export const addLog = log => async dispatch => {
     dispatch({
       type: ADD_LOG,
       payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+//delete log from server
+export const deleteLog = id => async dispatch => {
+  try {
+    setLoading();
+    await fetch(`/logs/${id}`, {
+      method: "DELETE"
+    });
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
     });
   } catch (err) {
     dispatch({
